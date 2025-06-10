cd5c8be59f0ac2b478dade875838c8b2e1465c88
dragoicho <gk@morphelectronics.com>
2025-06-10 11:17:46 -0400

    added type hinting

--- Work/sears.py
+++ Work/sears.py
@@ -1,9 +1,11 @@
 # sears.py
+''' Exercise 1.6: Debugging '''
 
-bill_thickness = 0.11 * 0.001    # Meters (0.11 mm)
-sears_height   = 442             # Height (meters)
-num_bills      = 1
-day            = 1
+
+bill_thickness: float = 0.11 * 0.001    # Meters (0.11 mm)
+sears_height: float = 442             # Height (meters)
+num_bills: float = 1
+day: int = 1
 
 while num_bills * bill_thickness < sears_height:
     print(day, num_bills, num_bills * bill_thickness)
