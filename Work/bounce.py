2a0cb5e38f87b8518f95caddd0ac4dc9d6502eb7
dragoicho <gk@morphelectronics.com>
2025-06-10 11:11:52 -0400

    made a real nice bounce thingy

--- Work/bounce.py
+++ Work/bounce.py
@@ -1,3 +1,13 @@
 # bounce.py
-#
-# Exercise 1.5
+''' Exercise 1.5 '''
+
+
+def rebound(initial_height: float | int) -> float:
+    ''' return the rebound height based on initial height '''
+    gravity: float = 3.0 / 5.0
+    return initial_height * gravity
+
+height: float = 100
+for bounce in range(1, 11):
+    height = round(rebound(height), 4)
+    print(bounce, height)
