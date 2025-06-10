7b89ebb0001ceec8833fae5f320908bce20bc629
dragoicho <gk@morphelectronics.com>
2025-06-10 11:25:25 -0400

    added module doc string, type hinting, assignment-math operatoir, and format strings

--- Work/mortgage.py
+++ Work/mortgage.py
@@ -1,3 +1,14 @@
 # mortgage.py
-#
-# Exercise 1.7
+''' Exercise 1.7 '''
+
+
+principal: float = 500_000.0
+rate: float = 0.05
+payment: float = 2684.11
+total_paid: float = 0.0
+
+while principal > 0:
+    principal *= (1 + (rate / 12)) - payment
+    total_paid += payment
+
+print(f'Total paid: {total_paid}')
