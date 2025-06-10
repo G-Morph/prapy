7d085ea91023f090b07277489d560cc1d23b2a9d
dragoicho <gk@morphelectronics.com>
2025-06-10 11:20:06 -0400

    fixed error with addition and multiplication assignment operators also, trimmed whitespace within code

--- Work/sears.py
+++ Work/sears.py
@@ -2,15 +2,15 @@
 ''' Exercise 1.6: Debugging '''
 
 
-bill_thickness: float = 0.11 * 0.001    # Meters (0.11 mm)
-sears_height: float = 442             # Height (meters)
+bill_thickness: float = 0.11 * 0.001  # Meters (0.11 mm)
+sears_height: float = 442  # Height (meters)
 num_bills: float = 1
 day: int = 1
 
 while num_bills * bill_thickness < sears_height:
     print(day, num_bills, num_bills * bill_thickness)
-    day = days + 1
-    num_bills = num_bills * 2
+    day += 1
+    num_bills *= 2
 
 print('Number of days', day)
 print('Number of bills', num_bills)
