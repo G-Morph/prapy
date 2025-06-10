9b9bec014d9088f7e9b97a33072ad5bb8f6f14e9
dragoicho <gk@morphelectronics.com>
2025-06-10 19:02:19 -0400

    added total and print methods as well as, oh yeah, making a Portfolio class

--- Work/pcost.py
+++ Work/pcost.py
@@ -2,26 +2,58 @@
 
 ''' Exercise 1.27 '''
 
+from csv import Error
 import os
 
 
-WORK: str = os.getcwd()
-CSVFILE: str = WORK + '\\Work\\Data\\portfolio.csv'
+class Porfolio():
+    ''' stock portfolio stuff '''
+
+    def __init__(self, portfolio_string: str):
+        self.portfolio_string = portfolio_string
+        self.parse_portfolio_string()
+
+    column_titles = None  # tuple of strings ('','','')
+    records: list[tuple[str, int, float]] = []
+
+    def parse_portfolio_string(self) -> None:
+        ''' split the CSV file into members '''
+        data_lines = data_string.split('\n')
+        self.column_titles = tuple(data_lines[0].split(','))
+        for line in data_lines[1:]:
+            record_items = line.split(',')
+            if len(record_items) == 3:
+                try:
+                    name: str = record_items[0]
+                    shares = int(record_items[1])
+                    price = float(record_items[2])
+                    self.records.append((name, shares, price))
+                except Error as e:
+                    raise ValueError("records should be: tuple[str, str, str]") from e
+
+    def total_value(self) -> float:
+        ''' total value from all shares '''
+        total_value: float = 0
+        for record in self.records:
+            _, _, stock_price = record
+            total_value += stock_price
+        # :.2f formats float string to 2 decimal places with 0 fill:
+        print(f"Total value:    ${round(total_value, 2):.2f}")
+        return total_value
+
+    def print_portfolio(self) -> None:
+        ''' print each stock's name, number of shares, and total value '''
+        for record in self.records:
+            name, shares, price = record
+            print(f"{name}\t {shares:>3}\t ${price:.2f}\t")
+
+
+CWD: str = os.getcwd()
+CSVFILE: str = CWD + '\\Work\\Data\\portfolio.csv'
 
 with open(CSVFILE, encoding='utf-8') as csvfile:
     data_string: str = csvfile.read()
 
-data_lines = data_string.split('\n')
-column_titles = tuple(data_lines[0].split(','))
-records: list[tuple[str, int, float]] = []
-for line in data_lines[1:]:
-    record_items = line.split(',')
-    if len(record_items) == 3:
-        name: str = record_items[0]
-        shares = int(record_items[1])
-        price = float(record_items[2])
-        records.append((name, shares, price))
-
-for record in records:
-    n, s, p = record
-    print(f"{n}\t {s:>3}\t ${p:.2f}\t")
+portfolio = Porfolio(data_string)
+portfolio.print_portfolio()
+portfolio_value: float = portfolio.total_value()
