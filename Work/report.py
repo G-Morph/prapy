588c07f35496b844df95bd63f1ccb91e8d348766
dragoicho <gk@morphelectronics.com>
2025-06-11 15:31:08 -0400

    updated make report for the exercises, updated the formatted printing at bottom, and make the update_portfolio() method, not yet needed

--- Work/report.py
+++ Work/report.py
@@ -34,44 +34,59 @@ def read_prices(csv_file: str) -> dict[str, float]:
     return stocks
 
 
-def calculate_gain(  # should be called update_portfolio()
+def update_portfolio(
     portfolio: list[dict[str, str|int|float]],
     stocks: dict[str, float]
-    ) -> tuple[float, float, float]:
-    ''' did you lose some or win some? also, update portfolio with new prices '''
-    prev_total: float = 0
-    current_total: float = 0
+    ) -> None:
+    ''' update portfolio with new prices '''
     for holding in portfolio:
-        # seems kludgey with all these float() casts
-        # update prev total, for calculating gain/loss
-        prev_total += float(holding['price']) * int(holding['shares'])
         if holding['name'] in stocks.keys():
             # update the holding dict with the new price
             holding['price'] = float(stocks[str(holding['name'])])
-            # update current total with new price
-            current_total += float(holding['price']) * int(holding['shares'])
-    return (prev_total, current_total, current_total - prev_total)
+
+
+def make_report(
+    portfolio: list[dict[str, str|int|float]],
+    stocks: dict[str, float]
+    ) -> list[tuple[str, int, float, float]]:
+    ''' print out a report on stock price changes within a specific portfolio '''
+    reports: list[tuple[str, int, float, float]] = []
+    for holding in portfolio:
+        if holding['name'] in stocks.keys():
+            name = str(holding['name'])
+            shares = int(holding['shares'])
+            old_price = float(holding['price'])
+            new_price = float(stocks[name])
+            reports.append((name, shares, new_price, round(new_price - old_price, 2)))
+    return reports
 
 
 # READ PORTFOLIO
 portfolio_ = read_portfolio('Work\\Data\\portfolio.csv')
-print(
-    'Name' + '\t' +
-    'Shares' + '\t' +
-    'Price')
-for holding_ in portfolio_:
-    print(
-        f'{holding_['name']}' + '\t' +
-        f'{holding_['shares']}' + '\t' +
-        f'${holding_['price']:.2f}')
 
 # READ PRICES:
 stocks_ = read_prices('Work\\Data\\prices.csv')
-print(f'IBM:\t${stocks_['IBM']:.2f}')
 
 # UPDATE AND CALCULATE GAIN:
-prev_total_, current_total_, gain_ = calculate_gain(portfolio_, stocks_)
-print(
-    f'Previous total: ${prev_total_:.2f}' + '\n'
-    f'Current total: ${current_total_:.2f}' + '\n'
-    f'Gain: ${gain_:.2f}')
+#prev_total_, current_total_, gain_ = calculate_gain(portfolio_, stocks_)
+#print(
+#    f'Previous total: ${prev_total_:.2f}' + '\n'
+#    f'Current total: ${current_total_:.2f}' + '\n'
+#    f'Gain: ${gain_:.2f}')
+
+# MAKE REPORT
+reports_ = make_report(portfolio_, stocks_)
+header = 'Name Shares Price Change'.split()
+border: str = '-' * 10
+print(f'{header[0]:>10} ' +
+      f'{header[1]:>10} ' +
+      f'{header[2]:>10} ' +
+      f'{header[3]:>10}')
+print(border, border, border, border)
+for report_ in reports_:
+    # you have to 'pre-format' the string otherwise it won't center with the dollar sign:
+    price = f'${report_[2]:.2f}'
+    print(f'{report_[0]:>10s} ' +
+          f'{report_[1]:>10d} ' +
+          f'{price:>10} ' +  # this is where the dollar string would get messed up
+          f'{report_[3]:>10.2f}')
