fa9bc5e17d8690b1d3da9faf8640a8f6b11e6be7
dragoicho <gk@morphelectronics.com>
2025-06-11 10:44:56 -0400

    finished exercise 2.4

--- Work/report.py
+++ Work/report.py
@@ -1,3 +1,28 @@
 # report.py
-#
-# Exercise 2.4
+''' Exercise 2.4 '''
+
+import csv
+
+
+def read_portfolio(filename: str) -> None:
+    ''' parse/read portfolio csv file '''
+
+    portfolio = []
+
+    with open(filename, 'rt', encoding='utf-8') as f:
+        rows = csv.reader(f)
+        headers = next(rows)
+        for row in rows:
+            holding = (row[0], int(row[1]), float(row[2]))
+            portfolio.append(holding)
+        print(
+            f'{headers[0]}' + '\t' +
+            f'{headers[1]}' + '\t' +
+            f'{headers[2]}')
+        for holding in portfolio:
+            print(
+                f'{holding[0]}' + '\t' +
+                f'{holding[1]}' + '\t' +
+                f'{holding[2]}')
+
+read_portfolio('Work\\Data\\portfolio.csv')
