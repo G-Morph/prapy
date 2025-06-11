91f0a4a028fd046a18b60db0054c42e82ce8f806
dragoicho <gk@morphelectronics.com>
2025-06-11 11:28:59 -0400

    niced up the printing of holdings

--- Work/report.py
+++ Work/report.py
@@ -7,7 +7,7 @@ import csv
 def read_portfolio(filename: str) -> None:
     ''' parse/read portfolio csv file '''
 
-    portfolio = []
+    portfolio: list[dict[str, str|int|float]] = []
 
     with open(filename, 'rt', encoding='utf-8') as f:
         rows = csv.reader(f)
@@ -15,11 +15,21 @@ def read_portfolio(filename: str) -> None:
         for row in rows:
             holding = {
                 headers[0]: row[0],
-                headers[1]: row[1],
-                headers[2]: row[2]}
+                headers[1]: int(row[1]),
+                headers[2]: float(row[2])}
             portfolio.append(holding)
 
+    print(
+        'Name' + '\t' +
+        'Shares' + '\t' +
+        'Price')
     for holding in portfolio:
-        print(holding)
+        name = holding['name']
+        shares = holding['shares']
+        price = holding['price']
+        print(
+            f'{name}' + '\t' +
+            f'{shares}' + '\t' +
+            f'${price:.2f}')
 
 read_portfolio('Work\\Data\\portfolio.csv')
