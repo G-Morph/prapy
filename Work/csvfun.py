5e3daa1a82d287d5bd90e3a1fb9e7b10eabb3eff
dragoicho <gk@morphelectronics.com>
2025-06-11 19:12:32 -0400

    commented stuff

--- Work/csvfun.py
+++ Work/csvfun.py
@@ -2,8 +2,21 @@
 
 import csv
 
+def extract_columns(filepath: str) -> list[dict[str, str]]:
+    ''' find the desired columns in a csv file '''
+    with open(filepath, 'rt', encoding='utf-8') as f:
+        rows = csv.reader(f)  # get each row of data
+        headers = next(rows)  # get the fist row, which contains header strings
+        select = ['name', 'shares', 'price']  # the columns you want
+        # where to find the columns you want:
+        indices = [ headers.index(col_name) for col_name in select ]
+        portfolio = [  # list comp
+            { col_name: row[index] for col_name, index in zip(select, indices) }  # dict comp
+            for row in rows ]
+        return portfolio
 
-with open('Work/Data/portfolio.csv', 'rt', encoding='utf-8') as f:
-    rows = csv.reader(f)
-    headers = next(rows)
-    print(headers)
+
+if __name__ == "__main__":
+    what_you_want = extract_columns('Work/Data/portfoliodate.csv')
+    for thing in what_you_want:
+        print(thing)
