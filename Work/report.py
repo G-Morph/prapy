122872eeb5a635fd6f88a2d09d09d359b4b88ea6
dragoicho <gk@morphelectronics.com>
2025-06-11 10:57:03 -0400

    using dictionaries instead of tuples for portfolio holdings

--- Work/report.py
+++ Work/report.py
@@ -13,16 +13,13 @@ def read_portfolio(filename: str) -> None:
         rows = csv.reader(f)
         headers = next(rows)
         for row in rows:
-            holding = (row[0], int(row[1]), float(row[2]))
+            holding = {
+                headers[0]: row[0],
+                headers[1]: row[1],
+                headers[2]: row[2]}
             portfolio.append(holding)
-        print(
-            f'{headers[0]}' + '\t' +
-            f'{headers[1]}' + '\t' +
-            f'{headers[2]}')
-        for holding in portfolio:
-            print(
-                f'{holding[0]}' + '\t' +
-                f'{holding[1]}' + '\t' +
-                f'{holding[2]}')
+
+    for holding in portfolio:
+        print(holding)
 
 read_portfolio('Work\\Data\\portfolio.csv')
