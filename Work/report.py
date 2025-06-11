bcf2f13bdfee6ac674f5a60ba0d3e7cca3e5cdab
dragoicho <gk@morphelectronics.com>
2025-06-11 12:09:40 -0400

    added the read_prices func and cleaned stuff up

--- Work/report.py
+++ Work/report.py
@@ -4,32 +4,50 @@
 import csv
 
 
-def read_portfolio(filename: str) -> None:
+def read_portfolio(csv_file: str) -> None:
     ''' parse/read portfolio csv file '''
 
     portfolio: list[dict[str, str|int|float]] = []
 
-    with open(filename, 'rt', encoding='utf-8') as f:
+    with open(csv_file, 'rt', encoding='utf-8') as f:
         rows = csv.reader(f)
-        headers = next(rows)
+        # this pops the header line, prior knowledge of file needed for this stuff:
+        next(rows)
+        # now get to the actual data, from the second line on:
         for row in rows:
             holding = {
-                headers[0]: row[0],
-                headers[1]: int(row[1]),
-                headers[2]: float(row[2])}
+                'name': row[0],
+                'shares': int(row[1]),
+                'price': float(row[2])}
             portfolio.append(holding)
-
+    # print it all out nice like:
     print(
         'Name' + '\t' +
         'Shares' + '\t' +
         'Price')
     for holding in portfolio:
-        name = holding['name']
-        shares = holding['shares']
-        price = holding['price']
         print(
-            f'{name}' + '\t' +
-            f'{shares}' + '\t' +
-            f'${price:.2f}')
+            f'{holding['name']}' + '\t' +
+            f'{holding['shares']}' + '\t' +
+            f'${holding['price']:.2f}')
 
+# call it to check it out:
 read_portfolio('Work\\Data\\portfolio.csv')
+
+
+def read_prices(csv_file: str) -> dict[str, float]:
+    ''' read prices of other stocks in a csv file '''
+
+    # 'name' = 'price'
+    stocks: dict[str, float] = {}
+
+    with open(csv_file, 'rt', encoding='utf-8') as f:
+        rows = csv.reader(f)
+        # no headers in this file (Work\Data\prices.csv)
+        for row in rows:
+            if row:  # 'name' = 'price'
+                stocks[row[0]] = float(row[1])
+    return stocks
+
+# call it to check it out:
+prices = read_prices('Work\\Data\\prices.csv')
