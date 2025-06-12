33cb27f2f6d32f3cdb28ba54b3e7f56d4607aee6
dragoicho <gk@morphelectronics.com>
2025-06-12 17:12:31 -0400

    added functionality for header/headerless csv also added beginning of functionality for delimiter

--- Work/fileparse.py
+++ Work/fileparse.py
@@ -5,9 +5,11 @@ import csv
 
 def parse_csv(
     filename: str,
+    has_header: bool = True,
+    delimiter: str = ',',
     select: list[str] | None = None,
     types: list[type] | None = None
-    ) -> list[dict]:
+    ) -> list:
     ''' parse a csv file into a list of records '''
 
     if select is None:
@@ -18,24 +20,31 @@ def parse_csv(
     records = []  # the output list
 
     with open(filename, encoding='utf-8') as f:
-        rows = csv.reader(f)
-        headers = next(rows)  # hopefully, there's a header row!
-        for row in rows:
-            if row:  # if not a blank line in the csv file
+        rows = csv.reader(f, delimiter=delimiter)
+        if has_header:
+            headers = next(rows)
+            for row in rows:
+                if row:  # if not a blank line in the csv file
+                    if types:  # if you need to cast strigs from the file
+                        # each col of the row is the arg for type() casting
+                        row = [typecast(col) for typecast, col in zip(types, row)]
+                    # make a dict of each row:
+                    raw_record = dict(zip(headers, row))
+                    # now make a new dict filtered by the select list arg:
+                    filtered_record = {}
+                    for key, value in raw_record.items():
+                        # if it's one of the parameters (col) you want:
+                        if key in select:
+                            # add it to the record you'll return
+                            filtered_record[key] = value
+                    # append it to the output list:
+                    records.append(filtered_record)
+        else:  # no header
+            for row in rows:
                 if types:  # if you need to cast strigs from the file
                     # each col of the row is the arg for type() casting
                     row = [typecast(col) for typecast, col in zip(types, row)]
-                # make a dict of each row:
-                raw_record = dict(zip(headers, row))
-                # now make a new dict filtered by the select list arg:
-                filtered_record = {}
-                for key, value in raw_record.items():
-                    # if it's one of the parameters (col) you want:
-                    if key in select:
-                        # add it to the record you'll return
-                        filtered_record[key] = value
-                # append it to the output list:
-                records.append(filtered_record)
+                records.append(row)
     return records
 
 
@@ -57,3 +66,9 @@ if __name__ == "__main__":
     )
     for line in res:
         print(line)
+
+
+## for a csv file with no headers:
+#res = parse_csv(
+#    "Work/"
+#)
