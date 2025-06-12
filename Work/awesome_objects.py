75bbfe7780ecccbeb4899fbd44f645bea12b1996
dragoicho <gk@morphelectronics.com>
2025-06-11 20:57:48 -0400

    made auto-cast loops and stuff like that, `cause types are objects too

--- Work/awesome_objects.py
+++ Work/awesome_objects.py
@@ -1,6 +1,10 @@
 ''' showcasing how everything in Python is a "first-class" object '''
 
+import csv
 import math
+
+
+TYPE, VALUE = (0, 1)
 
 
 objects = [abs, math, ValueError]
@@ -20,3 +24,26 @@ try:
     # pylint: enable=invalid-name
 except objects[2] as ve:
     print(ve)
+
+
+def parse_portfolio_string(filepath: str) -> list:
+    ''' split the CSV file into members '''
+    records = []
+    with open(filepath, 'rt', encoding='utf-8') as f:
+        rows = csv.reader(f)
+        header = next(rows)
+        types = [str, int, float]
+        for index, row in enumerate(rows):
+            if row:
+                values = zip(types, row)
+                record = dict(zip(header, values))
+                try:
+                    name = record['name'][TYPE](record['name'][VALUE])  # cast as string
+                    shares = record['shares'][TYPE](record['shares'][VALUE])  # cast as int
+                    price = record['price'][TYPE](record['price'][VALUE])  # cast as float
+                    records.append((name, shares, price))
+                except ValueError as ve:
+                    raise ValueError(f"Row {index}: Couldn't convert: {row}") from ve
+    return records
+
+print(parse_portfolio_string('Work/Data/portfolio.csv'))
