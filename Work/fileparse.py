2cdd23078b78e2666db0caba164f554ee1caec4d
dragoicho <gk@morphelectronics.com>
2025-06-11 22:15:04 -0400

    added user filtering functionality

--- Work/fileparse.py
+++ Work/fileparse.py
@@ -3,8 +3,13 @@
 import csv
 
 
-def parse_csv(filename: str) -> list:
+def parse_csv(
+    filename: str,
+    select = None
+    ) -> list:
     ''' parse a csv file into a list of records '''
+    if select is None:
+        select = ['name', 'shares']
     records = []
     with open(filename, encoding='utf-8') as f:
         rows = csv.reader(f)
@@ -12,11 +17,23 @@ def parse_csv(filename: str) -> list:
         for row in rows:
             if row:
                 record = dict(zip(headers, row))
-                records.append(record)
+                filtered_record = {}
+                for key, value in record.items():
+                    if key in select:
+                        filtered_record[key] = value
+                records.append(filtered_record)
     return records
 
 
 if __name__ == "__main__":
     print(parse_csv(
-        "Work/Data/portfolio.csv"
+        "Work/Data/portfolio.csv",
+        select=['name', 'price']
+    ))
+    print(parse_csv(
+        "Work/Data/portfolio.csv",
+        select=['shares']
+    ))
+    print(parse_csv(
+        "Work/Data/portfolio.csv",
     ))
