616652a7de7ce76b4d69047669f0b09528915cc0
dragoicho <gk@morphelectronics.com>
2025-06-11 20:10:25 -0400

    fleshed out the demo

--- Work/awesome_objects.py
+++ Work/awesome_objects.py
@@ -5,3 +5,18 @@ import math
 
 objects = [abs, math, ValueError]
 print(objects)
+
+great_number: int = -420
+abs_res: int = objects[0](great_number)
+print(abs_res)
+
+it_takes: int = 2
+famous_number: float = objects[1].sqrt(it_takes)
+print(famous_number)
+
+try:
+    # pylint: disable=invalid-name
+    x = int("obvo not an integer")
+    # pylint: enable=invalid-name
+except objects[2] as ve:
+    print(ve)
