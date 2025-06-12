fd5f9196b4e0914e3a7591e70e6c62815409912e
dragoicho <gk@morphelectronics.com>
2025-06-11 21:52:15 -0400

    working on exercise 3.4, created main simple func

--- Work/fileparse.py
+++ Work/fileparse.py
@@ -1,3 +1,22 @@
-# fileparse.py
-#
-# Exercise 3.3
+''' Exercise 3.3 '''
+
+import csv
+
+
+def parse_csv(filename: str) -> list:
+    ''' parse a csv file into a list of records '''
+    records = []
+    with open(filename, encoding='utf-8') as f:
+        rows = csv.reader(f)
+        headers = next(rows)
+        for row in rows:
+            if row:
+                record = dict(zip(headers, row))
+                records.append(record)
+    return records
+
+
+if __name__ == "__main__":
+    print(parse_csv(
+        "Work/Data/portfolio.csv"
+    ))
